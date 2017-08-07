d169dac49db88f4cae09c123952adc27697b82c7
Jeongseop Yi <yjs990427@gmail.com>
2017-08-06 21:40:42 -0400

    class Tile code review

--- tile.cpp
+++ tile.cpp
@@ -1,36 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "tile.h"
 
 using namespace std;
+/// Constructors
+// No variables
 Tile::Tile()
 {
 	this->_col = 0;
 	this->_row = 0;
-	//this->_revealed = true;
 }
 
+// with variables
 Tile::Tile(int col, int row)
 {
 	this->_col = col;
 	this->_row = row;
-	//this->_revealed = true;
 }
 
-Tile::Tile(Tile& t)
-{
-	this->_mine = t.isMine();
-	this->_revealed = t.isRevealed();
-	this->_flagged = t.isFlagged();
-
-	this->_neighborCount = t.returnNeighborCount();
-	this->_done = t.isDone();
-	
-	this->_col = t.returnCol();
-	this->_row = t.returnRow();
-}
-
+// print the tile status
 void Tile::printTile()
 {
 	std::cout << "|";
@@ -41,22 +31,11 @@ void Tile::printTile()
 	else cout << "C";									// Covered 
 }
 
-void Tile::reveal()
-{
-	this->_revealed = true;
-}
-
-// is series
+/// is series
 // is it mine?
 bool Tile::isMine()
 {
 	return this->_mine;
-}
-
-// is it Flagged?
-bool Tile::isFlagged()
-{
-	return this->_flagged;
 }
 
 // is it revealed?
@@ -65,23 +44,23 @@ bool Tile::isRevealed()
 	return this->_revealed;
 }
 
-// is it done? (AI) 
-bool Tile::isDone()
+// is it Flagged?
+bool Tile::isFlagged()
 {
-	return this->_done;
+	return this->_flagged;
 }
 
-// return series
-// return its column
-int Tile::returnCol()
-{
-	return this->_col;
-}
-
+/// return series
 // return its row
 int Tile::returnRow()
 {
 	return this->_row;
+}
+
+// return its column
+int Tile::returnCol()
+{
+	return this->_col;
 }
 
 // return neighborCount;
@@ -90,7 +69,7 @@ int Tile::returnNeighborCount()
 	return this->_neighborCount;
 }
 
-// set series
+/// set series
 // set Mine
 void Tile::setMine()
 {
@@ -104,19 +83,47 @@ void Tile::setFlag()
 	else this->_flagged = true;
 }
 
-void Tile::setFlag(bool set)
-{
-	this->_flagged = set;
-}
-
 // set NeighborCount
 void Tile::setNeighborCount(int count)
 {
 	this->_neighborCount = count;
 }
 
+// set reveal
+void Tile::setReveal()
+{
+	this->_revealed = true;
+}
+
+/// for Client
+// copy constructor
+Tile::Tile(Tile& t)
+{
+	this->_mine = t.isMine();
+	this->_revealed = t.isRevealed();
+	this->_flagged = t.isFlagged();
+
+	this->_neighborCount = t.returnNeighborCount();
+	this->_done = t.isDone();
+	
+	this->_col = t.returnCol();
+	this->_row = t.returnRow();
+}
+
+// is it done? (AI) 
+bool Tile::isDone()
+{
+	return this->_done;
+}
+
 // set done
 void Tile::setDone(bool done)
 {
 	this->_done = done;
+}
+
+// set flag to which I want
+void Tile::setFlag(bool set)
+{
+	this->_flagged = set;
 }
--- tile.h
+++ tile.h
@@ -3,35 +3,43 @@
 class Tile
 {
 private:
+	// universal
 	bool _mine = false;				// is it mine?
 	bool _revealed = false;			// is it revealed?
 	bool _flagged = false;
 
-	int _neighborCount = 0;				// how many mines does the neighbors have?
+	int _neighborCount = 0;			// how many mines near the tile?
 	int _col;
 	int _row;
-	bool _done = false;
+
+	// for Client (AI)
+	bool _done = false;				// AI finished?
 
 public:
+	// universal
 	Tile();
-	Tile(Tile& t);
 	Tile(int col, int row);
 
-	void reveal();
 	void printTile();
 
 	bool isMine();
 	bool isRevealed();
 	bool isFlagged();
-	bool isDone();
-
+	
 	int returnRow();
 	int returnCol();
 	int returnNeighborCount();
 
 	void setMine();
 	void setFlag();
-	void setFlag(bool set);
 	void setNeighborCount(int count);
+	void setReveal();
+
+	// for Client
+	Tile(Tile& t);
+
+	bool isDone();
+
+	void setFlag(bool set);
 	void setDone(bool done);
 };
