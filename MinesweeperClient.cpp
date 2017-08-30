90a53ed27cfcefaf819e577333cdaa97951c9d0f
Jeongseop Yi <yjs990427@gmail.com>
2017-08-30 14:06:31 -0400

    exception + sleep for 40ms

--- MinesweeperClient.cpp
+++ MinesweeperClient.cpp
@@ -1,28 +1,28 @@
-// MinesweeperAI.cpp : Defines the entry point for the console application.
-//
-
 #include <iostream>
-
-#include "minesweeper.h"
-#include "client.h"
-#include "json.hpp"
-#include "AI.h"
-
 #include <boost/thread/thread.hpp>
+
+#include "json.hpp"
+#include "minesweeper.h"
+#include "AI.h"
+#include "client.h"
 
 using namespace std;
 using json = nlohmann::json;
 
 void GameLoop(json* info, json* to_send, MineSweeper *m)
 {
-	m->printMineField();
-	AI(m, to_send);
-	boost::this_thread::sleep(boost::posix_time::milliseconds(100));
-	ingameTransfer(to_send, info, m);
+	while (!(*info)["GameEnd"])
+	{
+		m->printMineField();
+		AI(m, to_send);
+		boost::this_thread::sleep(boost::posix_time::milliseconds(40));
+		ingameTransfer(to_send, info, m);
+	}
 }
 
 int main()
 {
+	try {
 	cout << "Minesweeper AI" << endl << endl;
 	int col, row, mineNum;
 	startGamefunction(col, row, mineNum);
@@ -31,12 +31,9 @@ int main()
 	json info, to_send;
 	firstScriptTransfer(col, row, info);
 	update(minefield, info);
-	
-	while (!info["GameEnd"])
-	{
-		GameLoop(&info, &to_send, minefield);
-	}
 
+	GameLoop(&info, &to_send, minefield);
+	
 	minefield->printMineField();
 	if (info["win"])
 	{
@@ -49,4 +46,10 @@ int main()
 	cin.ignore();
 	cin.get();
 	return 0;
+	}
+	catch (std::exception& e) {
+		cout << e.what() << endl;
+		cin.ignore();
+		cin.get();
+	}
 }
